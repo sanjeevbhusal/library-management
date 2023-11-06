dfff5ee74a8ce349476d26af9edd3cf04d501d58
sanjeevbhusal <bhusalsanjeev23@gmail.com>
2023-11-06 10:56:55 +0545

    fix next-auth issue

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
