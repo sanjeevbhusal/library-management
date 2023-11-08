e4ec77a733d19fe8f81de7db7988ff17ba46741c
Sanjeev Bhusal <bhusalsanjeev23@gmail.com>
2023-11-08 08:18:22 +0000

    upload navbar links and formatting

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -7,6 +7,9 @@ import { BiSolidBookOpen } from "react-icons/bi";
 import { FiUser } from "react-icons/fi";
 import { signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
 
 const navLinks = [
   {
@@ -15,14 +18,17 @@ const navLinks = [
     route: "/all-books",
   },
   {
+    label: "Your Books",
+    route: "/your-library",
+    adminOnly: true,
+  },
+  {
     label: "Upload Book",
-    icon: AiOutlineCloudUpload,
     route: "/upload-book",
     adminOnly: true,
   },
   {
-    label: "Your Library",
-    icon: FiUser,
+    label: "Manage Books",
     route: "/your-library",
     adminOnly: true,
   },
@@ -33,34 +39,56 @@ interface Props {
 }
 
 function NavBar({ session }: Props) {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
-    <div className="flex justify-between items-center  bg-sky-400  py-4 px-4 lg:px-16 fixed left-0 right-0 top-0">
+    <div className="flex justify-between items-center border-b py-4 px-4 lg:px-16 fixed left-0 right-0 top-0">
       <Link href="/">
         <h1 className="font-semibold text-sm lg:text-lg">Library</h1>
       </Link>
       {session?.user ? (
-        <div className="flex gap-4 items-center">
+        <div>
           {navLinks.map((navLink) => {
             if (navLink.adminOnly && session.user.isAdmin != true) return null;
 
-            const Icon = navLink.icon;
-
             return (
               <Link href={navLink.route} key={navLink.label}>
-                <div className="flex gap-2 items-center cursor-pointer text-xs lg:text-base">
-                  <Icon />
+                <Button
+                  size="sm"
+                  variant="ghost"
+                  className={cn("text-gray-500 font-normal relative", {
+                    "text-black": pathname.includes(navLink.route),
+                  })}
+                >
+                  {navLink.label}
+                  {pathname.includes(navLink.route) && (
+                    <Separator className="absolute top-[50px]  bg-black w-[calc(100%-24px)] h-[2px]" />
+                  )}
+                </Button>
+                {/* <div className="flex gap-2 items-center cursor-pointer text-xs lg:text-base">
                   <span className="text-base">{navLink.label}</span>
-                </div>
+                </div> */}
               </Link>
             );
           })}
 
-          <Button size="sm" onClick={() => signOut()}>
-            Signout
+          <Button
+            size="sm"
+            variant="ghost"
+            className="text-gray-500 font-normal"
+            onClick={() => signOut()}
+          >
+            Logout
           </Button>
         </div>
       ) : (
-        <Button size="sm" onClick={() => signIn("google")}>
+        <Button
+          size="sm"
+          variant="ghost"
+          className="text-gray-500 font-normal"
+          onClick={() => signIn("google")}
+        >
           Login
         </Button>
       )}
